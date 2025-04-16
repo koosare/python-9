0bacb07e92184970a5231b0da738b55062fbf0e6
koosare <4370032@mail.ru>
2025-04-16 19:30:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ import os
 from pytimeparse import parse
 
 
-load_dotenv()
-CHAT_ID = os.environ['CHAT_ID']
-TOKEN = os.environ['TOKEN']
-BOT = ptbot.Bot(TOKEN)
+
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
@@ -19,26 +16,30 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def notify_progress(secs_left, message_id, message):  
-    BOT.update_message(CHAT_ID, message_id, "осталось {} секунд \n {}".format(secs_left,render_progressbar(message,message - secs_left)  ))
+def notify_progress(chat_id, secs_left, message_id, message):  
+    bot.update_message(chat_id, message_id, "осталось {} секунд \n {}".format(secs_left,render_progressbar(message,message - secs_left)  ))
 
 
-def wait(CHAT_ID, question):
+def wait(chat_id, question):
     time = parse(question)
-    message_id = BOT.send_message(CHAT_ID,  "Запускаю таймер...")
-    BOT.create_countdown( time, notify_progress, message_id=message_id, message=time)
-    BOT.create_timer(time, choose, CHAT_ID=CHAT_ID, message=time) 
+    message_id = bot.send_message(chat_id,  "Запускаю таймер...")
+    bot.create_countdown( time, notify_progress, message_id=message_id, message=time)
+    bot.create_timer(time, choose, chat_id=chat_id, message=time) 
 
 
-def choose(CHAT_ID, message): 
-    BOT.send_message(CHAT_ID, "Время вышло!")
+def choose(message, chat_id): 
+    bot.send_message(chat_id, "Время вышло!")
 
 
 def main():
-    BOT.send_message(CHAT_ID, "Бот запущен")
-    BOT.reply_on_message(wait)
-    BOT.run_bot()
+    chat_id = os.environ['CHAT_ID']
+    bot.send_message(chat_id, "Бот запущен")
+    bot.reply_on_message(wait)
+    bot.run_bot()
     
 
 if __name__ == '__main__':
+    load_dotenv()
+    token = os.environ['TOKEN']
+    bot = ptbot.Bot(token)
     main()
